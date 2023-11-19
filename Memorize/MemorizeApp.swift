12eee3fcba3c4ee721ca46f1bd93c5560bd0d7c3
Travis Peebles <travis@peebles.family>
2023-11-18 21:11:41 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Travis Peebles on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
