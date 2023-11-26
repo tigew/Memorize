2c2a3ea2e6d5f5fa839d43fefefda587507825cd
Travis Peebles <travis@peebles.family>
2023-11-25 23:26:20 -0600

    Making my rect better in light and dark

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 12)
+                .strokeBorder(lineWidth: 2)
             Text("🥶").font(.largeTitle)
         }
         .foregroundColor(.orange)
-        .imageScale(.small)
         .padding()
     }
 }
