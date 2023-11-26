76a6e731e044b8768957d0087b892c10f86580c3
Travis Peebles <travis@peebles.family>
2023-11-25 22:58:11 -0600

    We got something here.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,10 +11,10 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
         }
+        .foregroundStyle(.tint)
+        .imageScale(.large)
         .padding()
     }
 }
