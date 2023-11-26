9e2fd0e81d97bcc90930b1e6499858b14ebe5370
Travis Peebles <travis@peebles.family>
2023-11-25 23:33:18 -0600

    Removing the View of a card we made into a CardView to make it easier and more pleasing on the eyes to manupilate

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,6 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 12)
                 .foregroundColor(.white)
@@ -16,8 +29,6 @@ struct ContentView: View {
                 .strokeBorder(lineWidth: 2)
             Text("🥶").font(.largeTitle)
         }
-        .foregroundColor(.orange)
-        .padding()
     }
 }
 
