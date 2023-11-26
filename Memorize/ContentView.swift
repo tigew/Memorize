4887e75b61cb6d848cc102134110889e71a3a45d
Travis Peebles <travis@peebles.family>
2023-11-25 23:42:13 -0600

    Adding a check to workout if the card is faceup or not and show visually

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
             CardView()
             CardView()
@@ -24,11 +24,15 @@ struct CardView: View {
     var isFaceUp: Bool = false
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
-            Text("🥶").font(.largeTitle)
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🥶").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
         }
     }
 }
