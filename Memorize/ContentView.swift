6569fe35b0d9aec60f23cf5306ab6bf03787fefe
Travis Peebles <travis@peebles.family>
2023-12-01 22:35:18 -0600

    Heavy simplification of body

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,17 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🥶", "👻", "🔫", "🎯"]
-    
+    let emojis = ["🥶", "👻", "🔫", "🎯", "🤪", "😀", "😂", "😊", "😎", "😍", "🤩", "😜", "😇", "🤗", "🤔"]
+    @State var cardCount: Int = 4
     
     var body: some View {
+        VStack {
+            cards
+            cardCountAdjuster
+        }
+        .padding()
+    }
+    
+    var cardRemover: some View {
+        Button(action: {
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        })
+    }
+    
+    var cards: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
-        }
-        .foregroundColor(.orange)
-        .padding()
+        }.foregroundColor(.orange)
+    }
+    
+    var cardCountAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }.imageScale(.large).font(.title)
+    }
+    
+    var cardAdder: some View {
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+        })
     }
 }
 
