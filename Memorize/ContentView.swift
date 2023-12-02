45fc6ef44c41fbc8b42c2384bd14e073e6e6ced1
Travis Peebles <travis@peebles.family>
2023-12-01 20:07:12 -0600

    temp game logic

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     
     var body: some View {
         ZStack {
@@ -35,6 +35,8 @@ struct CardView: View {
             } else {
                 base.fill()
             }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
         }
     }
 }
